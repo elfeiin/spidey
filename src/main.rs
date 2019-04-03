a355440ab3de91602670c413de5e9ba9a1fe35ef
Jared Landers <patlymlatun@gmail.com>
2019-04-02 22:46:28 -0700

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,59 @@ mod canvas;
 mod cmd;
 mod pointer;
 mod parser;
-mod display;
+
+enum Message {
+	Close,
+	Draw(Vec<cmd::Command>),
+	Width(usize),
+	Height(usize),
+	Continue,
+}
+
 fn main() {
-	// The canvas we will draw on
 	let mut main_canvas = canvas::Canvas::new(4, 4);
-	// The 2d pointer we will use to draw on the canvas
 	let mut main_pointer = pointer::Pointer::new(&main_canvas);
 	loop {
-		// Uses terminal input for now
 		let mut input = String::new();
+		
 		io::stdin().read_line(&mut input).expect("failed to read line");
 		input = input.trim().to_string();
-		if (input == "q") || (input ==  "qq") || (input ==  "quit") || (input ==  "exit") {
-			break
+		match handle_input(input) {
+			Message::Close => break,
+			Message::Draw(mut commands) => {
+				main_canvas.blank();
+				main_pointer.blank(&main_canvas);
+				cmd::run(&mut commands, &mut main_canvas, &mut main_pointer);
+			},
+			Message::Width(n) => {
+				main_canvas.set_width(&mut main_pointer, n);
+			},
+			Message::Height(n) => {
+				main_canvas.set_height(&mut main_pointer, n);
+			},
+			Message::Continue => (),
 		}
-		// Modify canvas size (terminal input)
-		if input.len() > 4 {
-			let split = input.split(" ").collect::<Vec<&str>>();
-			let num = parser::parse_num(&split[1]).0;
-			match split[0] {
-				"width" => {main_canvas.set_width(&mut main_pointer,num);continue},
-				"height" => {main_canvas.set_height(&mut main_pointer,num);continue},
-				_ => ()
-			}
-		}
-		// Reinitialize the canvas buffer to black ([0_u8;4])
-		main_canvas.blank();
-		// Reinitialize the pointer: Good as new()!
-		main_pointer.blank(&main_canvas);
-		// parser::parse_text() returns a vec of type cmd::Command
-		let mut commands = parser::parse_text(&input);
-		// Bring it all together and run the commands
-		cmd::run(&mut commands, &mut main_canvas, &mut main_pointer);
-		// Display an abbreviation in terminal
-		display::display_canvas(&main_canvas);
+		draw(&main_canvas);
 	}
+}
+
+fn handle_input(i: String) -> Message {
+	let split = i.split(" ").collect::<Vec<&str>>();
+	let n = parser::parse_num(&split[1]).0;
+	match split[0] {
+		"q" => Message::Close,
+		"qq" => Message::Close,
+		"quit" => Message::Close,
+		"exit" => Message::Close,
+		"width" => Message::Width(n),
+		"height" => Message::Height(n),
+		"" => Message::Continue,
+		" " => Message::Continue,
+		"continue" => Message::Continue,
+		_ => Message::Draw(parser::parse_text(&i)),
+	}
+}
+
+fn draw(c: &canvas::Canvas) {
+	
 }
